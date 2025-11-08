f4e868c4a0d2386c138a9b247e748c0156a445d6
adham <adham.ferchichi@esprit.tn>
2025-11-08 20:14:51 +0100

    implémentation css

--- weatherProject/weatherApp/views.py
+++ weatherProject/weatherApp/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render , HttpResponse
+from django.shortcuts import render, HttpResponse
 import requests
 import datetime
+from django.contrib import messages
+from requests.exceptions import RequestException
+
 
 # Create your views here.
 
-def home (request) :
-    return HttpResponse ("hey this is my django server")
+def home(request):
+    return HttpResponse("hey this is my django server")
 
 def home2 (request) :
     if 'city' in request.POST :
@@ -14,12 +17,18 @@ def home2 (request) :
         city = 'Tunis'
 
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=45e80778880ca791836345a2f4ca0e39'
-    PARAMS = {'unitd': 'metric'}
+    PARAMS = {'units': 'metric'}
 
-    data = requests.get(url,PARAMS).json()
-    description = data['weather'][0]['description']
-    icon = data['weather'][0]['icon']
-    temp = data['main']['temp'] - 273.15
-    day = datetime.date.today
-    
-    return render (request, 'index.html', { 'description': description, 'icon': icon, 'temp': temp, 'city': city, 'day': day })
+    try :
+        data = requests.get(url,PARAMS).json()
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = round(data['main']['temp'], 1)
+        day = datetime.date.today()
+        return render (request, 'index.html', { 'description': description, 'icon': icon, 'temp': temp, 'city': city, 'day': day, 'exception_occured' : False })
+    except KeyError:
+        exception_occurrred = True
+        message.error (request, 'data entrée est non valide pour API')
+        day = datetime.date.today()
+        return render (request, 'index.html', {'description':'clear sky', 'icon':'01d'  ,'temp':25 , 'day':day , 'city':'indore' , 'exception_occurred':exception_occurred } )
+               
--- weatherProject/weatherProject/settings.py
+++ weatherProject/weatherProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -60,6 +61,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
+                'django.template.context_processors.static',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -115,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
